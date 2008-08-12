bc53a3fce1419b7b5e0417789db41a4d80f651b0
Denis Borovikov <denis.borovikov@gmail.com>
2008-08-12 11:39:37 +0000

    Added development implementation of Software RAID
    git-svn-id: svn+ssh://svn.code.sf.net/p/inq/svn/trunk/client/lib/einarc@1251 74654560-fcd8-4db3-b3c5-4c63e7cb38b4

--- src/raid/software.rb
+++ src/raid/software.rb
@@ -48,7 +48,7 @@ module RAID
 			File.open('/proc/mdstat', 'r') { |f|
 				f.each_line { |l|
 					l.chop!
-					p l
+#					p l
 					case l
 					when /^md(\d+)\s*:\s*(\S+)\s*(\S+)\s*(.*)$/
 						num = $1.to_i
@@ -74,8 +74,54 @@ module RAID
 			return @logical
 		end
 
-		def logical_add(raid_level, discs = nil, sizes = nil, options = nil)
-			raise NotImplementedError
+		def logical_add(raid_level, discs = nil, sizes = nil)
+			# Normalize arguments: make "discs" and "sizes" an array, "raid_level" a string
+			if discs
+				discs = discs.split(/,/) if discs.respond_to?(:split)
+				discs = [discs] unless discs.respond_to?(:each)
+			end
+			raid_level = raid_level.to_s
+			
+			# Replace SCSI enumerations by devices
+			discs.map!{|d| scsi_to_device(d) }
+			
+			puts "DEBUG: #{discs.inspect}"
+			
+			# Checking disk for free
+			for d in discs
+				raise Error.new("Device #{d} is allready in RAID") if raid_member?(d)
+			end
+			
+			raise Error.new('Software RAID does not support setting of the sizes') if sizes
+			
+			# If no discs use all free devices
+			if discs.nil?
+				discs = devices.select{|d| not raid_member?(d) }
+				rise Error.new('No free disks') if discs.empty?
+			end
+
+#linear, raid0, 0, stripe, raid1, 1, mirror, raid4, 4, raid5,  5,  raid6, 6,  raid10,  10, multipath, mp, faulty
+
+			case raid_level.downcase
+			when 'passthrough'
+				# Creating passthrough disc
+				raise Error.new('Passthrough requires exactly 1 physical disc') unless discs.size == 1
+				raid_level = 'linear'
+			when '5'
+				raise Error.new('RAID 5 requires more than 3 discs') unless discs.size > 2
+			end
+			
+			# Unmount all devices
+			discs.each{|d| `umount "#{d}" 2>/dev/null` }
+			
+			# Run mdadm command to create RAID
+			`mdadm --create --verbose #{next_raid_device_name} --auto=yes #{devices.size == 1 ? '--force' : ''} --level=#{raid_level} --raid-devices=#{devices.size} #{devices.join(' ')}`
+			
+			# Save configuration
+			# TODO:
+			
+			# Refresh lists
+			@raids = @devices = nil
 		end
 
 		def logical_delete(id)
@@ -87,7 +133,38 @@ module RAID
 		end
 
 		def _physical_list
-			raise NotImplementedError
+			# Find all storage udis by hal
+			udis = `hal-find-by-property  --key storage.drive_type --string disk`.split("\n")
+			
+			# Find all raid udis
+			excluded_udis = `hal-find-by-capability --capability storage.linux_raid`.split("\n")
+
+			# Delete all raid udis from udi list
+			for udi in excluded_udis
+				udis.delete(udi)
+			end
+			
+			# Resulting hash
+			res = {}
+			
+			# Extracting data from hal
+			for udi in udis
+				d = {}
+				d[:model] = `hal-get-property --udi #{udi} --key storage.model`.chomp
+				d[:size] = `hal-get-property --udi #{udi} --key storage.size`.to_i / 1000000000.0
+				d[:serial] = `hal-get-property --udi #{udi} --key storage.serial`.chomp
+				d[:revision] = `hal-get-property --udi #{udi} --key storage.firmware_version`.chomp
+				device = `hal-get-property --udi #{udi} --key block.device`.chomp
+				if raid_member?(device)
+					d[:state] = 'RAID Member'
+				else
+					d[:state] = 'Free'
+				end
+				target = phys_to_scsi(device.gsub(/^\/dev\//, ''))
+				res[target] = d
+			end
+			
+			res
 		end
 
 		# ======================================================================
@@ -129,10 +206,54 @@ module RAID
 		def parse_physical_string(str)
 			res = []
 			str.split(/ /).each { |ph|
-				p ph
+#				p ph
 				res[$2.to_i] = phys_to_scsi($1) if ph =~ /^(.+)\[(\d+)\]$/
 			}
 			return res
+		end
+		
+		def list_devices
+			# Find all storage udis by hal
+			udis = `hal-find-by-property  --key storage.drive_type --string disk`.split("\n")
+			
+			# Find all raid udis
+			excluded_udis = `hal-find-by-capability --capability storage.linux_raid`.split("\n")
+
+			# Delete all raid udis from udi list
+			for udi in excluded_udis
+				udis.delete(udi)
+			end
+			
+			udis.map{|udi| `hal-get-property --udi #{udi} --key block.device`.chomp }
+			#['dev/sdb', '/dev/sdc']
+		end
+		
+		def devices
+			@devices ||= list_devices
+		end
+		
+		def raid_member?(device)
+			raid_udis = `hal-find-by-capability --capability storage.linux_raid`.split("\n")
+			busied_udis = []
+			for udi in raid_udis
+				busied_udis += `hal-get-property --udi #{udi} --key storage.linux_raid.components`.split(' ')
+			end
+			busied_devices = busied_udis.map{|udi| `hal-get-property --udi #{udi} --key block.device`.chomp }
+			busied_devices.include?(device)
+		end
+		
+		def list_raids
+			raid_udis = `hal-find-by-capability --capability storage.linux_raid`.split("\n")
+			raid_udis.map{|udi| `hal-get-property --udi #{udi} --key block.device`.chomp }
+		end
+		
+		def raids
+			@raids ||= list_raids
+		end
+		
+		def next_raid_device_name
+			last_id = raids.map{|dev| dev.gsub(/\/dev\/md/, '').to_i }.sort[-1]
+			last_id.nil? ? "/dev/md0" : "/dev/md#{last_id + 1}"
 		end
 
 		# Converts physical name (sda) to SCSI enumeration (1:0)
@@ -148,6 +269,16 @@ module RAID
 				res = name
 			end
 			return res
+		end
+		
+		# Converts SCSI enumeration to physical device name
+		def scsi_to_device(id)
+			parts = id.split(':')
+			res = "/dev/hd" if parts[0] == '1'
+			res = "/dev/sd" if parts[0] == '0'
+			res += ('a'[0] + parts[1].to_i).chr
+			res += parts[2].to_s if parts.size == 3
+			res
 		end
 	end
 end
