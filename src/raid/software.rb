809620d4fab14b4e86e9d28fbc00b2e772a5aea2
Sergey Matveev <stargrave@stargrave.org>
2011-06-09 13:12:13 +0000

    Additional way of determining if is it an USB device
    git-svn-id: svn+ssh://svn.code.sf.net/p/inq/svn/trunk/client/lib/einarc@1890 74654560-fcd8-4db3-b3c5-4c63e7cb38b4

--- src/raid/software.rb
+++ src/raid/software.rb
@@ -469,7 +469,8 @@ module RAID
 
 		def usb_device?(device)
 			name = device.gsub(/^\/dev\//, '')
-			return (not [File.read("/sys/block/#{name}/uevent")].grep(/usb/).empty?)
+			return ((not [File.read("/sys/block/#{name}/uevent")].grep(/usb/).empty?) or 
+			       (File.readlink( "/sys/block/#{name}" ) =~ /usb/))
 		end
 
 		def active?(device)
