35051860be710f7319e930109eea8ff254f3fc66
Sergey Matveev <stargrave@stargrave.org>
2011-10-11 13:42:36 +0000

    Fixed get_logical_writecache behaviour: get_physical_writecache's result is string
    git-svn-id: svn+ssh://svn.code.sf.net/p/inq/svn/trunk/client/lib/einarc@1958 74654560-fcd8-4db3-b3c5-4c63e7cb38b4

--- src/raid/software.rb
+++ src/raid/software.rb
@@ -396,7 +396,7 @@ module RAID
 		def get_logical_writecache(num)
 			ld = _logical_list.reject { |logical| logical[:num] != num.to_i }[0]
 			raise Error.new("Unknown logical disc \"#{num}\"") unless ld
-			ld[:physical].collect { |pd| get_physical_writecache pd }.count(0) == ld[:physical].length ? 0 : 1
+			ld[:physical].collect { |pd| get_physical_writecache pd }.count("0") == ld[:physical].length ? 0 : 1
 		end
 
 		def set_logical_writecache_0(num)
